6ecf28908f5c5c4d0ace71861e31c4b78946e2fa
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2025-05-25 17:40:18 +0000

    upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import jest from 'eslint-plugin-jest';
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(['**/dist/', '**/*.d.ts']), {
+    extends: compat.extends('eslint:recommended'),
+
+    plugins: {
+        jest,
+    },
+
+    languageOptions: {
+        globals: {
+            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, 'off'])),
+            ...globals.node,
+            ...jest.environments.globals.globals,
+        },
+
+        parser: babelParser,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+
+    rules: {
+        'no-var': 'warn',
+        quotes: ['warn', 'single'],
+    },
+}]);
